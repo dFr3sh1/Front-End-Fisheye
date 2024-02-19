36f26c6fcef4f7c5b4dd76d5931735279e48ab42
DFr3sh <dfr3sh1@protonmail.com>
2024-02-19 20:52:44 +0100

    Modified function getUserCardDOM and added the const necessary to complete the object, modified names of photographers portraits for webp and photos performed, modified function getPhotographers in index.js to call the the data from the photographers.json file, index.html displayes photographes at the screen

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,38 +1,45 @@
 //Mettre le code JavaScript lié à la page photographer.html
+//Recuperate data from json file
+
 
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price} = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/photographersIDPhotos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement('article');
         const img = document.createElement('img');
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
         const h2 = document.createElement('h2');
         h2.textContent = name;
-        let locationDiv = document.createElement('div')
-        const city = document.createElement('city');
-        city.textContent = city;
-        const country = document.createElement('country');
-        country.textContent = country;
-        const tagline = document.createElement('tagline');
-        tagline.textContent = tagline;
-        const price = document.createElement('price')
-        price.textContent = tagline + '/jour';
+
+        let locationDiv = document.createElement('div');
+        const cityElement = document.createElement('p');
+        cityElement.textContent = city;
+        const countryElement = document.createElement('p');
+        countryElement.textContent = country;
+        const taglineElement = document.createElement('p');
+        taglineElement.textContent = tagline;
+        const priceElement = document.createElement('p');
+        priceElement.textContent = `${price} €/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
-        article.appendChild(locationDiv)
-        locationDiv.appendChild(city)
-        locationDiv.appendChild(country)
-        article.appendChild(tagline)
-        article.appendChild(price)  
-        return (article);
+        article.appendChild(locationDiv);
+        locationDiv.appendChild(cityElement);
+        locationDiv.appendChild(countryElement);
+        article.appendChild(taglineElement);
+        article.appendChild(priceElement);
+        return article;
+    }
+    return { name, picture, getUserCardDOM }
+    
     }
     
 
-    return { name, picture, getUserCardDOM }
-}
+console.log(photographerTemplate)
+
 export default photographerTemplate
 
 //Class constructor@
