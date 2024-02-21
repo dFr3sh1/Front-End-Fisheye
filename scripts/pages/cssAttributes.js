2982775ebb16c22511517ffb5dfad9f007f9e755
DFr3sh <dfr3sh1@protonmail.com>
2024-02-21 20:18:30 +0100

    Created cssAttribute.js and modified css attributes for articles tag

---
+++ scripts/pages/cssAttributes.js
@@ -0,0 +1,6 @@
+//File to add css selectors and arrtibutes
+
+const portraitDiv = document.querySelector('.photographer_section')
+const articleAttributes = document.querySelectorAll('.portrait-class')
+
+
