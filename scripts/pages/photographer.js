b7bf7aa2185903bcd4da6d8d9a72433b5e099e28
DFr3sh <dfr3sh1@protonmail.com>
2024-02-27 15:33:31 +0100

    Created layouts articles, created variables.scss and adjusted css homepage

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -10,37 +10,36 @@ function photographerTemplate(data) {
     function getUserCardDOM() {
         const article = document.createElement('article');
         const img = document.createElement('img');
-        const imgShadow = document.createElement('div')
-        imgShadow.classList.add('imgShadow')
         //img.setAttribute("src", picture);
-        img.classList.add('portrait-class')
+        img.classList.add('portrait-class');
         img.style.backgroundImage = `url(assets/photographers/photographersIDPhotos/${portrait})`;
         const h2 = document.createElement('h2');
         h2.textContent = name;
-        h2.classList.add('ph-name')
-
+        h2.classList.add('ph-name');
+        const ph_div_info = document.createElement('div');
+        ph_div_info.classList.add('ph-div-info');
         let locationDiv = document.createElement('div');
         locationDiv.classList.add('ph-country-div')
         const cityElement = document.createElement('p');
         cityElement.textContent = city;
-        cityElement.classList.add('ph-city')
+        cityElement.classList.add('ph-city');
         const countryElement = document.createElement('p');
         countryElement.textContent = country;
-        countryElement.classList.add('ph-country')
+        countryElement.classList.add('ph-country');
         const taglineElement = document.createElement('p');
         taglineElement.textContent = tagline;
         const priceElement = document.createElement('p');
         priceElement.textContent = `${price} €/jour` ?? ('Le photographe négocie son prix selon travail');
-        priceElement.classList.add('priceText')
+        priceElement.classList.add('priceText');
 
         article.appendChild(img);
-        article.appendChild(imgShadow)
         article.appendChild(h2);
-        article.appendChild(locationDiv);
+        article.appendChild(ph_div_info)
+        ph_div_info.appendChild(locationDiv);
         locationDiv.appendChild(cityElement);
         locationDiv.appendChild(countryElement);
-        article.appendChild(taglineElement);
-        article.appendChild(priceElement);
+        ph_div_info.appendChild(taglineElement);
+        ph_div_info.appendChild(priceElement);
 
 
         return article;
