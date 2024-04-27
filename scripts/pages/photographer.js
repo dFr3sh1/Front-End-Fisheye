594d35b3d7d5afa2e502ee34f318f67d3d1d4a47
DFr3sh <dfr3sh1@protonmail.com>
2024-04-27 15:20:55 +0200

    Realized the photographe information of photographes of photographe.html, added two more variables, country and city

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -35,11 +35,19 @@ function displayHeaderPh(photographe) {
     divHeader.classList.add('divHeader')
     const h1 = document.createElement('h1');
     h1.textContent = photographerModel.name;
-    const ph_country_div = photographerModel.getUserCardDOM().querySelector('.ph-country-div');
+    const ph_country_div = document.createElement('div')
+    ph_country_div.classList.add('ph-country-div')
+    const city = photographerModel.getUserCardDOM().querySelector('.ph-city')
+    city.textContent = photographerModel.city
+    city.classList.add('h2photographeLocation')
+    const country = photographerModel.getUserCardDOM().querySelector('.ph-country')
+    country.textContent = photographerModel.country
+    country.classList.add('h2photographeLocation')
     const tagline = document.createElement('p');
     tagline.textContent = photographerModel.tagline;
+    tagline.classList.add('contactTagline')
     const contactBtn = document.createElement('button');
-    contactBtn.classList.add('contactBtn')
+    contactBtn.classList.add('mainBtn', 'button')
     contactBtn.textContent = "Contactez-moi"
     const photographePortrait = photographerModel.getUserCardDOM().querySelector('.portrait-class');
     photographePortrait.classList.add('headerPortrait');
@@ -49,11 +57,13 @@ function displayHeaderPh(photographe) {
     
 
     // Ensure the retrieved elements exist before appending them
-    if (divHeader || h1 || ph_country_div || tagline || contactBtn || photographePortrait) {
+    if (divHeader || h1 || ph_country_div || city || country || tagline || contactBtn || photographePortrait) {
         // Append the retrieved elements to the new page
-        photographer.appendChild(divHeader); // Clone the elements to preserve attributes
+        photographer.appendChild(divHeader); //Append to the photographer div
         divHeader.appendChild(h1);
         divHeader.appendChild(ph_country_div); // Clone the elements to preserve attributes
+        ph_country_div.appendChild(city);
+        ph_country_div.appendChild(country);
         divHeader.appendChild(tagline); // Clone the elements to preserve attributes
         photographer.appendChild(contactBtn);
         photographer.appendChild(photographePortrait);
