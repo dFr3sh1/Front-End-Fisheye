de833cc8e3c954f342ac4d4556f51abaa7850049
DFr3sh <dfr3sh1@protonmail.com>
2024-06-14 16:40:12 +0200

    Organized the code

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -4,7 +4,7 @@
 
 class PhotographerTemplate {
     constructor(data) {
-        this.id = data.id
+        this.id = data.id;
         this.name = data.name;
         this.portrait = data.portrait;
         this.city = data.city;
@@ -13,36 +13,42 @@ class PhotographerTemplate {
         this.price = data.price;
     }
 
-   // const { name, portrait, city, country, tagline, price} = data;
-
-    //const picture = `assets/photographers/photographersIDPhotos/${this.portrait}`;
-
-
     getUserCardDOM() {
         const article = document.createElement('article');
+
         const a = document.createElement('a');
-        a.classList.add('a-link')
+        a.classList.add('a-link');
         a.href = `photographer.html?id=${this.id}`;
+
         const divLink = document.createElement('div');
-        divLink.id = 'divLink'
+        divLink.id = 'divLink';
+
         const img = document.createElement('img');
         img.classList.add('portrait-class');
         img.style.backgroundImage = `url(assets/photographers/photographersIDPhotos/${this.portrait})`;
+        img.alt = `Portrait de ${this.name}`;
+        
         const h2 = document.createElement('h2');
         h2.textContent = this.name;
         h2.classList.add('ph-name');
+
         const ph_div_info = document.createElement('div');
         ph_div_info.classList.add('ph-div-info');
+
         const locationDiv = document.createElement('div');
-        locationDiv.classList.add('ph-country-div')
+        locationDiv.classList.add('ph-country-div');
+
         const cityElement = document.createElement('p');
         cityElement.textContent = this.city;
         cityElement.classList.add('ph-city');
+
         const countryElement = document.createElement('p');
         countryElement.textContent = this.country;
         countryElement.classList.add('ph-country');
+
         const taglineElement = document.createElement('p');
         taglineElement.textContent = this.tagline;
+
         const priceElement = document.createElement('p');
         priceElement.textContent = `${this.price} €/jour` ?? ('Le photographe négocie son prix selon travail');
         priceElement.classList.add('priceText');
@@ -51,6 +57,7 @@ class PhotographerTemplate {
         a.appendChild(divLink);
         divLink.appendChild(img);
         divLink.appendChild(h2);
+
         article.appendChild(ph_div_info);
         ph_div_info.appendChild(locationDiv);
         locationDiv.appendChild(cityElement);
@@ -62,7 +69,5 @@ class PhotographerTemplate {
 
     }
 }
-
-console.log(PhotographerTemplate)
 
 export default PhotographerTemplate
